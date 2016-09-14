914fb965b4ea7c43a9fdd589a40c2af69bae1381
Debbie Milburn <debbiemilburn2@gmail.com>
2016-09-14 09:36:51 -0400

    Create new wolf form

--- app/controllers/wolf_controller.rb
+++ app/controllers/wolf_controller.rb
@@ -7,7 +7,21 @@ get '/wolves' do
   erb :'wolves/index'
 end
 
+get '/wolves/new' do
+  erb :'wolves/new'
+end
+
 get '/wolves/:id' do
   @wolf = Wolf.find(params[:id])
   erb :'wolves/show'
+end
+
+post '/wolves' do
+  wolf = Wolf.new(params[:wolf])
+  if wolf.save
+    redirect '/'
+  else
+    @errors = wolf.errors.full_messages
+    erb :'wolves/new'
+  end
 end
--- app/models/wolf.rb
+++ app/models/wolf.rb
@@ -1,3 +1,4 @@
 class Wolf < ActiveRecord::Base
+  validates :name, presence: true
   # Remember to create a migration!
 end
